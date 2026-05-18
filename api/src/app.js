6d28b91120e693ddf196b3babf625217af51c70a
Erik Dietrich <eb.dietrich11@gmail.com>
2026-05-18 17:56:54 -0400

    Add logger

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { httpLog } from "./util/logger.js";
 
 // Basic setup
 const env = process.env.NODE_ENV || "dev";
 
 // Express setup
 const app = express();
+app.use(httpLog);
 app.use(express.json());
 
 export default app;
---
+++ api/src/util/logger.js
@@ -0,0 +1,25 @@
+import pino from "pino";
+import pinoHttp from "pino-http";
+import pinoPretty from "pino-pretty";
+
+// Base config
+const baseConfig = {};
+
+// Pretty output for terminal
+const terminalConfig = {
+  transport: { target: "pino-pretty" },
+  options: {
+    colorize: true,
+  },
+};
+
+const config = process.stdout.isTTY //https://github.com/pinojs/pino-pretty#programmatic-integration
+  ? { ...baseConfig, ...terminalConfig }
+  : baseConfig;
+
+const log = pino(config);
+
+const httpLog = pinoHttp(config);
+
+export { httpLog };
+export default log;
