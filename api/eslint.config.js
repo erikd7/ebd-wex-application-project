015dd15fd8558ef7abc5411aa1cf8188861d0167
Erik Dietrich <eb.dietrich11@gmail.com>
2026-05-19 15:07:25 -0400

    Add pretter config and linter; fix lint errors

---
+++ api/eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "no-console": "error",
+    },
+  },
+];
